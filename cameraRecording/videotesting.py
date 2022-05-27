aee11e4e3f68174af1675042f897ce1ec0f75849
Lan Nguyen <74268571+nguyentlan@users.noreply.github.com>
2022-05-27 12:25:30 -0700

    added camera recording

--- cameraRecording/videotesting.py
+++ cameraRecording/videotesting.py
@@ -1,10 +1,15 @@
 import cv2
 import numpy as np
+from datetime import datetime
+# https://www.asus.com/support/FAQ/1039488/
    
 # Create a VideoCapture object and read from input file
 # cap = cv2.VideoCapture('test.avi')
 cap = cv2.VideoCapture(0)
-   
+
+# For recording the video 
+out = cv2.VideoWriter('outpy.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (1080, 720))
+
 # Check if camera opened successfully
 if (cap.isOpened()== False): 
   print("Error opening video  file")
@@ -17,9 +22,16 @@ while(cap.isOpened()):
   if ret == True:
    
     frame = cv2.resize(frame, (1080, 720))
+    # Show the time
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame, str(datetime.now()), (10,30), font, 1,(0,0,0),2,cv2.LINE_AA)
+    
     # Display the resulting frame
     cv2.imshow('Frame', frame)
-   
+
+    # For recording video  
+    out.write(frame)
+
     # Press Q on keyboard to  exit
     if cv2.waitKey(25) & 0xFF == ord('q'):
       break
@@ -31,6 +43,7 @@ while(cap.isOpened()):
 # When everything done, release 
 # the video capture object
 cap.release()
-   
+out.release()
+
 # Closes all the frames
 cv2.destroyAllWindows() 
