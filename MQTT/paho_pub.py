ee44fc11efdeb8e4143c05e23f3d94c2f434d77f
Ketan Joshi <ketanjoshi1224@gmail.com>
2022-10-14 18:31:35 -0700

    Added keyboard input command to the publisher

--- MQTT/paho_pub.py
+++ MQTT/paho_pub.py
@@ -1,8 +1,11 @@
 
 import random
 import time
-
+import sys
 from paho.mqtt import client as mqtt_client
+from pynput import keyboard
+from pynput.keyboard import Listener
+
 
 
 #broker = "local-ip"
@@ -32,15 +35,31 @@ def connect_mqtt():
 def publish(client):
     msg_count = 0
     while True:
-        time.sleep(1)
-        msg = f"messages: {msg_count}"
-        result = client.publish(topic, msg)
-        status = result[0]
-        if status == 0:
-            print(f"Send {msg} to topic {topic}")
-        else:
-            print(f"Failed to send message to topic {topic}")
-        msg_count += 1
+        #Will go thru a mode of listening to Key presses
+        if(sys.argv[1] == '0'):
+            def on_press(key):
+                key_result = format("Key Pressed: {0}".format(key))
+                print(key_result)
+                client.publish(topic, key_result)
+
+            def on_release(key):
+                print("Key Pressed: ".format(key))
+
+                #Stop the Program with ESC
+                if key == keyboard.Key.esc:
+                    return False
+            with Listener(on_press=on_press, on_release=on_release) as listener:
+                listener.join()
+        elif(sys.argv[1] == '1'):
+            time.sleep(1)
+            msg = f"messages: {msg_count}"
+            result = client.publish(topic, msg)
+            status = result[0]
+            if status == 0:
+                print(f"Send {msg} to topic {topic}")
+            else:
+                print(f"Failed to send message to topic {topic}")
+            msg_count += 1
 
 
 def run():
